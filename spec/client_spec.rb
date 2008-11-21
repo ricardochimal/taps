383cf2501ac4d3ab6e72296b2770aeb246b3d18a
Adam Wiggins <adam@heroku.com>
2008-11-21 03:49:29 -0800

    bacon allows should ==, which is prettier

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,11 +8,11 @@ describe Taps::Client do
 
 	it "opens the local db connection via sequel and the database url" do
 		Sequel.expects(:connect).with('sqlite://my.db').returns(:con)
-		@client.db.should.equal :con
+		@client.db.should == :con
 	end
 
 	it "creates a restclient resource to the remote server" do
-		@client.server.url.should.equal 'http://example.com:3000'
+		@client.server.url.should == 'http://example.com:3000'
 	end
 end
 
