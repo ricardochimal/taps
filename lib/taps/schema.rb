5e3f7d5c7c430664083cd6fa46dd052632df6a05
Ricardo Chimal, Jr <ricardo@heroku.com>
2009-01-27 18:57:00 -0800

    added bin/schema to load and dump the schema using activerecord

---
+++ lib/taps/schema.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+gem 'activerecord', '>= 2.2.2'
+gem 'activesupport', '>= 2.2.2'
+require 'active_record'
+require 'active_support'
+require 'stringio'
+require 'uri'
+
+module Taps
+module Schema
+	extend self
+
+	def create_config(url)
+		uri = URI.parse(url)
+		config = {
+			'adapter' => (uri.scheme == 'postgres') ? 'postgresql' : uri.scheme,
+			'database' => uri.path.blank? ? uri.host : uri.path.split('/')[1],
+			'username' => uri.user,
+			'password' => uri.password,
+			'host' => uri.host,
+		}
+	end
+
+	def connection(database_url)
+		config = create_config(database_url)
+		ActiveRecord::Base.establish_connection(config)
+	end
+
+	def dump(database_url)
+		connection(database_url)
+
+		stream = StringIO.new
+		ActiveRecord::SchemaDumper.ignore_tables = []
+		ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
+		stream.string
+	end
+
+	def load(database_url, schema)
+		connection(database_url)
+		eval(schema)
+	end
+end
+end
