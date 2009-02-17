415d11e848fa9a74cf137379d4feebb10f64f48c
Ricardo Chimal, Jr <ricardo@heroku.com>
2009-02-17 14:57:52 -0800

    code format config.rb

--- lib/taps/config.rb
+++ lib/taps/config.rb
@@ -10,21 +10,20 @@ module Taps
 		"#{version_yml[:major]}.#{version_yml[:minor]}.#{version_yml[:patch]}"
 	end
 
+	class Config
+		class << self
+			attr_accessor :taps_database_url
+			attr_accessor :login, :password, :database_url, :remote_url
+			attr_accessor :chunksize
 
-class Config
-	class << self
-		attr_accessor :taps_database_url
-		attr_accessor :login, :password, :database_url, :remote_url
-		attr_accessor :chunksize
-
-		def verify_database_url
-			db = Sequel.connect(self.database_url)
-			db.tables
-			db.disconnect
-		rescue Object => e
-			puts "Failed to connect to database:\n  #{e.class} -> #{e}"
-			exit 1
+			def verify_database_url
+				db = Sequel.connect(self.database_url)
+				db.tables
+				db.disconnect
+			rescue Object => e
+				puts "Failed to connect to database:\n  #{e.class} -> #{e}"
+				exit 1
+			end
 		end
 	end
-end
 end
