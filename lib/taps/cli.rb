146dd2242f4aec273d2857ab8a6f14bc8b56e69b
Thorben Schröder <stillepost@gmail.com>
2011-01-26 03:59:43 +0100

    Adds exclude_tables filter to exclude certain tables by name
    The filter option uses Regular Expressions which make it extremely hard to
    exclude a set of tables from a pull/push. Therefore a new option
    (-e or --exclude_tables=A,B,C) was added to exclude certain tables by name.

--- lib/taps/cli.rb
+++ lib/taps/cli.rb
@@ -137,6 +137,7 @@ EOHELP
         r_tables = v.collect { |t| "^#{t}$" }.join("|")
         opts[:table_filter] = "(#{r_tables})"
       end
+      o.on("-e", "--exclude_tables=A,B,C", Array, "Shortcut to exclude a list of tables") {|v| opts[:exclude_tables] = v}
       o.on("-d", "--debug", "Enable Debug Messages") { |v| opts[:debug] = true }
       o.parse!(argv)
 
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,4 +7,10 @@ describe Taps::Cli do
     opts = @cli.clientoptparse(:pull)
     opts[:table_filter].should == "(^mytable1$|^logs$)"
   end
+  
+  it "translates a list of tables to exclude into a regex that can be used in table_filter" do
+    @cli = Taps::Cli.new(["-e", "mytable1,logs", "sqlite://tmp.db", "http://x:y@localhost:5000"])
+    opts = @cli.clientoptparse(:pull)
+    opts[:exclude_tables].should == ['mytable1','logs']
+  end
 end
