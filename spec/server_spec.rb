7e4e967a0e2aa2a85f53e4d8e2c51dab2d1b4f3b
David Dollar <ddollar@gmail.com>
2011-04-01 11:30:25 -0400

    use the latest version for the spec check for taps version

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -23,7 +23,7 @@ describe Taps::Server do
   end
 
   it "verifies the client taps version" do
-    get('/', { }, { 'HTTP_AUTHORIZATION' => @auth_header, 'HTTP_TAPS_VERSION' => Taps.compatible_version + ".14" })
+    get('/', { }, { 'HTTP_AUTHORIZATION' => @auth_header, 'HTTP_TAPS_VERSION' => Taps.version })
     last_response.status.should == 200
   end
 
